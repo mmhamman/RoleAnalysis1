08f6cbe386c8e3df08f521eaa70155b7ecbd13d5
Hammans, Matthew Michael <mmhamman@iu.edu>
2024-01-31 14:06:40 -0500

    Physics and movement
    Physics calculations such as drag and gravity have now been implemented.
    
    Player input also added.
    
    Error occurring where playing falls through floor even though a collider is present on both objects.

--- RoleAnalysis1/Assets/Script/PlayerMovement.cs
+++ RoleAnalysis1/Assets/Script/PlayerMovement.cs
@@ -5,8 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public const gravity = 9.8f;
 
+    [Header("Player Movement")]
+    public InputAction movementInput;
+    public InputAction jumpInput;
+    public InputAction zipInput;
+    public InputAction swingInput;
+
+    [Header("Constants")]
+    public float gravity = 9.8f;
+    public float drag = 0.5f;
+
+    [Header("Player Movement")]
     public float speed = 5.0f;
     public float xSpeed = 0.0f;
     public float ySpeed = 0.0f;
@@ -20,6 +30,46 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position += new Vector3(xSpeed, ySpeed, 0);
+    }
+
+    void LateUpdate()
+    {
+        ySpeed -= gravity * Time.deltaTime;
+
+        // terminal velocity
+        if (ySpeed < -gravity)
+        {
+            ySpeed = -gravity;
+        }
+
+        xSpeed *= drag;
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+
+        float movement = context.ReadValue<float>();
+        xSpeed += movement * Time.deltaTime * speed;
+    }
+
+    void OnEnable()
+    {
+        movementInput.Enable();
+        jumpInput.Enable();
+        zipInput.Enable();
+        swingInput.Enable();
+
+        movementInput.performed += OnMovement;
+    }
+
+    void OnDisable()
+    {
+        movementInput.Disable();
+        jumpInput.Disable();
+        zipInput.Disable();
+        swingInput.Disable();
+
+        movementInput.performed -= OnMovement;
     }
 }
