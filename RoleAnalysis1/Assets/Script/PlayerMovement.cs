96f09490c1042243279826508c1d6093a6a4e055
Hammans, Matthew Michael <mmhamman@iu.edu>
2024-01-31 16:19:48 -0500

    Bug fixes and error handing

--- RoleAnalysis1/Assets/Script/PlayerMovement.cs
+++ RoleAnalysis1/Assets/Script/PlayerMovement.cs
@@ -20,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
 
 
     [Header("Player Movement")]
+    public bool isMoving = false;
     public bool isGrounded = false;
     public float speed = 5.0f;
     public float maxSpeed = 10.0f;
@@ -35,32 +36,73 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        OnMovement(movementInput);
+        OnMovement();
 
-        gameObject.GetComponent<CharacterController>().Move(new Vector3(xSpeed, ySpeed, 0));
+        gameObject.GetComponent<CharacterController>().Move(new Vector3(xSpeed, ySpeed, 0) * Time.deltaTime);
     }
 
     void LateUpdate()
     {
-        ySpeed -= gravity * Time.deltaTime;
+        // applying gravity
+        ySpeed -= gravity;
 
         // terminal velocity
         if (ySpeed < -gravity)
         {
             ySpeed = -gravity;
         }
+        
+        // capping max speed
+        if(xSpeed > maxSpeed) {
+            xSpeed = maxSpeed;
+        } else if(xSpeed < -maxSpeed) {
+            xSpeed = -maxSpeed;
+        }
 
+        // applying drag
+        if (isGrounded)
+        {
+            ApplyDrag(drag);
+        }
+        else
+        {
+            ApplyDrag(dragAir);
+        }
+    }
+
+    public void OnMovement()
+    {
+        float movement = movementInput.ReadValue<float>();
+
+        if (movement != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        xSpeed += movement * speed;
+        Debug.Log("Movement: " + xSpeed);
+    }
+
+    public void ApplyDrag(float drag)
+    {
+        if (isMoving)
+        {
+            return;
+        }
+
+        // applying drag
+        bool isNegativeDrag = xSpeed < 0;
+        xSpeed = Mathf.Abs(xSpeed);
         xSpeed *= drag;
+        xSpeed = isNegativeDrag ? -xSpeed : xSpeed;
 
         if (xSpeed < dragThreshold && xSpeed > -dragThreshold) {
             xSpeed = 0.0f;
         }
-    }
-
-    public void OnMovement(InputAction context)
-    {
-        float movement = context.ReadValue<float>();
-        xSpeed += movement * Time.deltaTime * speed;
     }
 
     void OnEnable()
