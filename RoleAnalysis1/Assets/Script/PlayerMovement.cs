f2924585fb98f5fb9dceaea64bba0bd0cbfa6bfd
Hammans, Matthew Michael <mmhamman@iu.edu>
2024-01-31 13:13:45 -0500

    Player added with variables
    gravity, speed and momentum variables are added for movement.

---
+++ RoleAnalysis1/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public const gravity = 9.8f;
+
+    public float speed = 5.0f;
+    public float xSpeed = 0.0f;
+    public float ySpeed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
